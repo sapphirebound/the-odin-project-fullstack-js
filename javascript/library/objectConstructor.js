ca22c9276999702f9fff4f6d2ddd783ced0c5f8f
Afif Roslan <afifroslan95@gmail.com>
2024-07-29 18:16:42 +0800

    -initialised html and css -created functions to add entries to the table

--- javascript/library/objectConstructor.js
+++ javascript/library/objectConstructor.js
@@ -1,9 +1,44 @@
 const myLibrary = [];
+const body = document.querySelector('body');
+const tbody = document
+    .getElementById('libraryTable')
+    .getElementsByTagName('tbody')[0];
 
-function Book() {
 
+function Book(id, title, author, pages) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
 }
 
-function addBookToLibrary() {
-
+function addBookToLibrary(id, title, author, pages) {
+    const entry = new Book(id, title, author, pages);
+    myLibrary.push(entry);
 }
+
+// test entry
+addBookToLibrary(0, '123', 'def', 123);
+
+// Function to auto-add delete button
+function actionButtons() {
+    const actionDiv = document.createElement("div");
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "delete";
+    actionDiv.appendChild(deleteButton);
+    return actionDiv;
+}
+
+// add entries to table
+myLibrary.forEach((entry) => {
+    let newRow = tbody.insertRow(entry.id);
+    let cellCount = 0;
+    for (const e in entry) {
+        let newCell = newRow.insertCell(cellCount);
+        newCell.textContent = entry[e];
+        cellCount += 1;
+    };
+    let actions = newRow.insertCell(cellCount);
+    actions.appendChild(actionButtons());
+}
+);
