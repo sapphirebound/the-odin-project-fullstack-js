9f29df24b5eb31a80ef2870fcd9a3473b491ece5
Afif Roslan <afifroslan95@gmail.com>
2024-07-30 17:25:06 +0800

    added function to update table when submitting form

--- javascript/library/objectConstructor.js
+++ javascript/library/objectConstructor.js
@@ -9,20 +9,20 @@ function Book(id, title, author, pages) {
     this.id = id;
     this.title = title;
     this.author = author;
-    this.pages = pages;
+    this.pages = Number(pages);
 }
 
 // create sample book to loop through
 const sampleBook = new Book('id', 'title', 'author', 'pages');
 
 function addBookToLibrary(title, author, pages) {
-    let bookID = myLibrary.length > 0 ? myLibrary.length + 1 : 0; //check if library length is not 0
+    let bookID = myLibrary.length > 0 ? myLibrary.length : 0; //check if library length is not 0
     const entry = new Book(bookID, title, author, pages);
     myLibrary.push(entry);
 }
 
 // test entry
-addBookToLibrary(]'123', 'def', 123);
+addBookToLibrary('123', 'def', 123);
 
 // Function to auto-add action buttons
 function actionButtons() {
@@ -49,14 +49,12 @@ for (property in Object.getOwnPropertyNames(sampleBook)) {
         continue;
     }
     else {
-    input += `<div><label for='${formKey}'>${formKey}</label><input type='text' id='${formKey}' name='${formKey}' /></div>`;
-}
+        input += `<label for='${formKey}'>${formKey}</label><input type='text' id='${formKey}' name='${formKey}' />`;
+    }
 }
 input += `<input type='submit' value='Add Book' />`;
 form.innerHTML = input;
 
-// submit button
-const formSubmit = document.createElement('input');
 
 //create dialog to contain form
 const formDialog = document.createElement('dialog');
@@ -70,21 +68,37 @@ addButton.addEventListener("click", (e) => {
     formDialog.show();
 });
 
+// submit button
+const formSubmit = document.querySelector('input[type="submit"]');
+
+// form submit
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); //disable sending data to server
+    const userTitle = document.querySelector('input#title').value;
+    const userAuthor = document.querySelector('input#author').value;
+    const userPages = document.querySelector('input#pages').value;
+    addBookToLibrary(userTitle, userAuthor, userPages);
+    updateTable();
+    form.reset();
+    formDialog.close();
+});
+
 // add entries to table
 function updateTable() {
     tbody.innerHTML = '';
-myLibrary.forEach((entry) => {
-    let newRow = tbody.insertRow(entry.id);
-    let cellCount = 0;
-    for (const e in entry) {
-        let newCell = newRow.insertCell(cellCount);
-        newCell.textContent = entry[e];
-        cellCount += 1;
-    };
-    let actions = newRow.insertCell(cellCount);
-    actions.appendChild(actionButtons());
-}
-);
+    myLibrary.forEach((entry) => {
+        let newRow = tbody.insertRow(entry.id);
+        let cellCount = 0;
+        for (const e in entry) {
+            let newCell = newRow.insertCell(cellCount);
+            newCell.textContent = entry[e];
+            cellCount += 1;
+        };
+        let actions = newRow.insertCell(cellCount);
+        actions.appendChild(actionButtons());
+    }
+    );
 }
 
 updateTable();
